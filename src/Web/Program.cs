b36fd7bb90ea29b525db3e97ea63fa5a623a7362
Roopa Kamath <kamathroopa06@gmail.com>
2024-10-19 21:17:49 +0100

    My Commit

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -22,6 +22,8 @@ using Microsoft.eShopWeb.Web.Pages;
 using Microsoft.FeatureManagement;
 using Microsoft.IdentityModel.Tokens;
 
+ // My first change
+ 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.AddConsole();
 
